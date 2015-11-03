8f163e33da0a634d34d9a1fab3b0eea2e2d3ebe3
Kristina <nosik94@list.ru>
2015-11-03 23:29:38 +0200

    d

--- resources/views/weather.php
+++ resources/views/weather.php
@@ -45,26 +45,43 @@ while ($current_rec = mysql_fetch_array($result));
 
 
  */
+    $town_id = DB::table('towns')->where('town', $town)->first();
+    $town_id = $town_id->id;      //получить id города
+
+    $date_today = (date("Y-m-d", strtotime("+0 day")));
+    $date_tomorrow = (date("Y-m-d", strtotime("+1 day")));
+    $date_after_tomorrow = (date("Y-m-d", strtotime("+2 day")));
 
 
-        $date_today = (date("Y-m-d", strtotime("+0 day")));
+    $weather_today = DB::table('weather')
+        ->where('town_id', $town_id)
+        ->whereBetween('kuupaev', [$date_today, $date_today.' 23:59:59'])
+        ->get();
+    $weather_tomorrow = DB::table('weather')
+        ->where('town_id', $town_id)
+        ->whereBetween('kuupaev', [$date_tomorrow, $date_tomorrow.' 23:59:59'])
+        ->get();
+    $weather_after_tomorrow = DB::table('weather')
+        ->where('town_id', $town_id)
+        ->whereBetween('kuupaev', [$date_after_tomorrow, $date_after_tomorrow.' 23:59:59'])
+        ->get();
 
-        $town_id = DB::table('towns')->where('town', $town)->first();
-        $town_id = $town_id->id;      //получить id города
-
-
-        $weather_today = DB::table('weather')
-            ->where('town_id', $town_id)
-            ->whereBetween('kuupaev', ['2015-11-02', '2015-11-02 23:59:59'])
-            ->get();
-  //  $weather_today = (array)$weather_today; // stdObject class ---> Array
 
     $weather_today = json_decode(json_encode((array) $weather_today), true);
-    print_r($weather_today);
- /*   $array = array("02.11.2015" => $weather_today
+    $weather_tomorrow = json_decode(json_encode((array) $weather_tomorrow), true);
+    $weather_after_tomorrow = json_decode(json_encode((array) $weather_after_tomorrow), true);
+
+
+    $array = array(
+        $date_today => $weather_today,
+        $date_tomorrow => $weather_tomorrow,
+        $date_after_tomorrow => $weather_after_tomorrow
     );
-print_r($array);*/
-       foreach($weather_today as $array){
+
+    // print_r($array);
+
+    echo "<table border='1'><tr>";
+   /*    foreach($weather_today as $array){
 
         //   $array = (array)$w;    // stdObject class ---> Array
            $temp_min = $array['temp_min'];
@@ -74,8 +91,22 @@ print_r($array);*/
 
 
            echo "Date: ".$date."<br> Min:".$temp_min."<br> Max: ".$temp_max."<br><p>";
-       }
+       }*/
 
+    foreach ($array as $key => $avalue) {
+
+        echo "<td>".$key."</td><td>";
+
+        for($i = 0; $i < count($avalue); $i++)
+            //echo '<td>'.$avalue[$i].'</td>';
+        print_r($avalue[$i]['temp_min']);
+
+
+        echo '<br /></td></tr>' ;
+    }
+
+
+    echo "</td></tr></table>";
 
 
 
