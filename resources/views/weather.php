8505719a681754d05e23de91700f34b6c156dd30
Kristina <nosik94@list.ru>
2015-11-04 22:48:43 +0200

    d

--- resources/views/weather.php
+++ resources/views/weather.php
@@ -7,44 +7,8 @@
     </head>
     <body>
 
-
-
-    <?php
- /*  1. Запросы на погоду, переносишь в метод show и передаёшь их из контроллера в вью.
-    2. На тему красоты есть варианты следующие.
-
-    Делаем ассоциативный массив, напимер weather
-    Где ключ будет дата, например 21 ноября а значение будет обьект из базы
-
-    В этот массив записываем результаты погоды за сегодня
-
-    За сегодня
-    За завтра
-    За после завтра
-
-    (Это тоже можно сделать в виде цикла но пока не заморачивайся и напиши отдельно по строчке на сегодня, завтра, после завтра)
-    (На этапе базы это будет 3 запроса)
-
-    Затем в view ты будешь
-    сначала делать первый первый цикл по ключам (вывести доступные дни)
-    И внутри цикла делать цикл что бы вывести погоду для этого конкретного дня.
-
-    как то так.
-    Спрашивай вопросы
-
-
- $query = "SELECT * FROM tb_test";
-$result = mysql_query($query) or die("Couldn't execute query!");
-$current_rec = mysql_fetch_array($result) ;
-do
-$i = $current_rec['id'];
-$MyCar[$i]['name'] = $current_rec['carname'];
-$MyCar[$i]['voditel'] = $current_rec['drivername'];
-$MyCar[$i]['dr_test'] = $current_rec['flag_test'];
-while ($current_rec = mysql_fetch_array($result));
-
-
- */
+    
+<?php
     $town_id = DB::table('towns')->where('town', $town)->first();
     $town_id = $town_id->id;      //получить id города
 
@@ -81,32 +45,12 @@ while ($current_rec = mysql_fetch_array($result));
     // print_r($array);
 
     echo "<table border='1'><tr>";
-   /*    foreach($weather_today as $array){
-
-        //   $array = (array)$w;    // stdObject class ---> Array
-           $temp_min = $array['temp_min'];
-           $temp_max = $array['temp_max'];
-           $date = $array['kuupaev'];
-
-
-
-           echo "Date: ".$date."<br> Min:".$temp_min."<br> Max: ".$temp_max."<br><p>";
-       }*/
-
     foreach ($array as $key => $avalue) {
-
-        echo "<td>".$key."</td><td>";
-
-        for($i = 0; $i < count($avalue); $i++)
-            //echo '<td>'.$avalue[$i].'</td>';
-        print_r($avalue[$i]['temp_min']);
-
-
-        echo '<br /></td></tr>' ;
+        echo "<td>" . date("d. F",strtotime($key)) . "</td>";
+        for ($i = 0; $i < count($avalue) - 1; $i++)
+            echo '<td>' . date("H:i",strtotime($avalue[$i]['kuupaev'])) . '<br/>Min: ' . $avalue[$i]['temp_min'] . '<br/>Max: ' . $avalue[$i]['temp_max'] . '</td><br /></td></tr>';
     }
-
-
-    echo "</td></tr></table>";
+    echo "</tr></table>";
 
 
 
