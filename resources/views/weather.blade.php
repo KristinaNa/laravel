2aa87413da9e799198caf669586b62ef099c296d
Kristina <nosik94@list.ru>
2015-11-08 20:45:12 +0200

    blade finished

--- resources/views/weather.blade.php
+++ resources/views/weather.blade.php
@@ -9,39 +9,33 @@
         </style>
     </head>
     <body>
-
         Hello, {{ $town }}. <p>
-            <div id="table">
+        <div id="table">
             @foreach ($array as $key => $avalue)
                 <div class="row">
-
-                <div class="col-md-2">
-
-                <table class="table table-bordered">
-                    <tr>
-                        <td colspan="3">{{date("d. F",strtotime($key))}}</td>
-                    </tr>
-                    <tr>
-                    <tr>
-                        <td>Время</td>
-                        <td>MIN</td>
-                        <td>MAX</td>
-                    </tr>
-                    </tr>
-
-                    @for ($i = 0; $i < count($avalue) - 1; $i++)
+                    <div class="col-md-2">
+                        <table class="table table-bordered">
                         <tr>
-                            <td>{{date("H:i",strtotime($avalue[$i]['kuupaev']))}}</td>
-                            <td>{{$avalue[$i]['temp_min']}}</td>
-                            <td>{{$avalue[$i]['temp_max']}}</td>
+                            <td colspan="3">{{date("d. F",strtotime($key))}}</td>
                         </tr>
-
-                    @endfor
-
-                </table>
-                </div>
-                    @endforeach
-                </div>
+                        <tr>
+                        <tr>
+                            <td>Время</td>
+                            <td>MIN</td>
+                            <td>MAX</td>
+                        </tr>
+                        </tr>
+                        @for ($i = 0; $i < count($avalue) - 1; $i++)
+                            <tr>
+                                <td>{{date("H:i",strtotime($avalue[$i]['kuupaev']))}}</td>
+                                <td>{{$avalue[$i]['temp_min']}}</td>
+                                <td>{{$avalue[$i]['temp_max']}}</td>
+                            </tr>
+                        @endfor
+                        </table>
+                    </div>
+            @endforeach
+        </div>
         </div>
     </body>
 </html>
